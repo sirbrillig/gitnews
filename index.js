742ebd97abcd927fb14bbbd576166b5724bca921
Payton Swick <payton@foolord.com>
2017-05-03 13:51:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const fetch = require( 'node-fetch' )
+const get = require( 'lodash.get' )
+require( 'dotenv' ).config()
+
+function getFormattedNotification( notification ) {
+	return notification.updated_at + ': ' + '(' + get( notification, 'repository.full_name' ) + ') ' + get( notification, 'subject.title' )
+}
+
+function output( line ) {
+	console.log( line )
+}
+
+const init = {
+	method: 'GET',
+	headers: {
+		'Authorization': 'token ' + process.env.GITNEWS_TOKEN,
+	},
+}
+fetch( 'https://api.github.com/notifications', init )
+.then( res => res.json() )
+.then( function( notifications ) {
+	notifications.map( getFormattedNotification ).map( output )
+} )
