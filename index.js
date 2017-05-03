faa8f2f5f85d5029af6af33fa3c9de17d3cf4c42
Payton Swick <payton@foolord.com>
2017-05-03 18:28:23 -0400

    Fetch and display notification urls

--- index.js
+++ index.js
@@ -1,37 +1,104 @@
 #!/usr/bin/env node
-const fetch = require( 'node-fetch' )
-const get = require( 'lodash.get' )
-require( 'dotenv' ).config()
-const chalk = require( 'chalk' )
-const date = require( 'date-fns' )
+const fetch = require( 'node-fetch' );
+const get = require( 'lodash.get' );
+require( 'dotenv' ).config();
+const chalk = require( 'chalk' );
+const date = require( 'date-fns' );
+const debugFactory = require( 'debug' );
+
+const debug = debugFactory( 'gitnews' );
+
+function getUrl( notification ) {
+	return get( notification, 'htmlUrl', '' );
+}
+
+function getUrlApiUrl( notification ) {
+	return get( notification, 'subject.url', '' );
+}
 
 function getDate( notification ) {
-	const now = Date.now()
-	return date.distanceInWords( now, date.parse( get( notification, 'updated_at', '' ) ), { addSuffix: true } )
+	const now = Date.now();
+	return date.distanceInWords(
+		now,
+		date.parse( get( notification, 'updated_at', '' ) ),
+		{ addSuffix: true }
+	);
 }
 
 function getRepo( notification ) {
-	return get( notification, 'repository.full_name', '' )
+	return get( notification, 'repository.full_name', '' );
 }
 
 function getTitle( notification ) {
-	return get( notification, 'subject.title', '' )
+	return get( notification, 'subject.title', '' );
 }
 
 function getFormattedNotification( note ) {
-	return chalk.bold.yellow( getDate( note ) + ': ' ) + chalk.green( '(' + getRepo( note ) + ') ' ) + getTitle( note )
+	debug( `formatting notification for ${ JSON.stringify( note.id ) }...` );
+	return [
+		chalk.bold.yellow( getDate( note ) + ': ' ),
+		chalk.green( '(' + getRepo( note ) + ') ' ),
+		getTitle( note ),
+		' -- ' + getUrl( note ),
+	].join( '' );
 }
 
 function output( line ) {
-	console.log( line )
+	console.log( line );
 }
 
-const init = {
-	method: 'GET',
-	headers: {
-		'Authorization': 'token ' + process.env.GITNEWS_TOKEN,
-	},
+function convertToJson( result ) {
+	return result.json();
 }
-fetch( 'https://api.github.com/notifications', init )
-.then( res => res.json() )
-.then( notifications => notifications.map( getFormattedNotification ).map( output ) )
+
+function getFormattedNotifications( notifications ) {
+	return notifications.map( getFormattedNotification );
+}
+
+function printNotifications( notifications ) {
+	debug( 'printing notifications...' );
+	getFormattedNotifications( notifications ).map( output );
+}
+
+function getFetchInit() {
+	return {
+		method: 'GET',
+		headers: {
+			Authorization: 'token ' + process.env.GITNEWS_TOKEN
+		}
+	};
+}
+
+function fetcNotifications() {
+	debug( 'fetching notifications...' );
+	return fetch( 'https://api.github.com/notifications', getFetchInit() );
+}
+
+function fetchNotificationSubjectUrl( notification ) {
+	const url = getUrlApiUrl( notification );
+	debug( `fetching notification url for ${ url }...` );
+	return fetch( url, getFetchInit() )
+		.then( convertToJson )
+		.then( subject => {
+			notification.htmlUrl = get( subject, 'html_url' );
+			return notification;
+		} );
+}
+
+function fetchNotificationSubjectUrls( notifications ) {
+	debug( 'fetching notification urls...' );
+	return Promise.all( notifications.map( fetchNotificationSubjectUrl ) );
+}
+
+function printError( err ) {
+	console.error( err );
+}
+
+// -------------
+
+fetcNotifications()
+	.then( convertToJson )
+	.then( fetchNotificationSubjectUrls )
+	.then( printNotifications )
+	.catch( printError );
+
