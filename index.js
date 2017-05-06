7486d77625f0b3c421c5003c6a12c9c9f27d9c73
Payton Swick <payton@foolord.com>
2017-05-06 14:33:33 -0400

    Convert to a library

--- index.js
+++ index.js
@@ -1,112 +1,43 @@
-#!/usr/bin/env node
 const fetch = require( 'node-fetch' );
 const get = require( 'lodash.get' );
-require( 'dotenv' ).config();
-const chalk = require( 'chalk' );
-const date = require( 'date-fns' );
-const meow = require( 'meow' );
-const inquirer = require( 'inquirer' );
-const Conf = require( 'conf' );
-const columnify = require( 'columnify' );
 
-const config = new Conf();
-
-let logMessages = false;
+let logFunction = () => null;
 
 function log( message ) {
-	logMessages && output( message );
-}
-
-function getUrl( notification ) {
-	return get( notification, 'htmlUrl', '' );
+	logFunction( message );
 }
 
 function getUrlApiUrl( notification ) {
 	return get( notification, 'subject.url', '' );
 }
 
-function getDate( notification ) {
-	const now = Date.now();
-	return date.distanceInWords(
-		now,
-		date.parse( get( notification, 'updated_at', '' ) ),
-		{ addSuffix: true }
-	);
-}
-
-function getRepo( notification ) {
-	return get( notification, 'repository.full_name', '' );
-}
-
-function getTitle( notification ) {
-	return get( notification, 'subject.title', '' );
-}
-
-function getFormattedNotification( note ) {
-	return {
-		date: chalk.bold.yellow( getDate( note ) + ': ' ),
-		repo: chalk.green( '(' + getRepo( note ) + ') ' ),
-		title: getTitle( note ),
-		urlSeparator: ' -- ',
-		url: chalk.green( getUrl( note ) ),
-	};
-}
-
-function output( line ) {
-	console.log( line );
-}
-
-function outputColumns( data ) {
-	const options = {
-		columns: [ 'date', 'repo', 'title', 'urlSeparator', 'url' ],
-		showHeaders: false,
-	};
-	output( columnify( data, options ) );
-}
-
 function convertToJson( result ) {
 	return result.json();
 }
 
-function getFormattedNotifications( notifications ) {
-	return notifications.map( getFormattedNotification );
-}
-
-function printNotifications( notifications ) {
-	log( `printing ${ notifications.length } notifications...` );
-	if ( notifications.length < 1 ) {
-		output( '👍  No notifications!' );
-		return;
-	}
-	outputColumns( getFormattedNotifications( notifications ) );
-}
-
-function getFetchInit() {
+function getFetchInit( token ) {
 	return {
 		method: 'GET',
 		headers: {
-			Authorization: 'token ' + getToken(),
+			Authorization: 'token ' + token,
 		},
 	};
 }
 
-function fetchNotifications() {
-	if ( ! getToken() ) {
+function fetchNotifications( token ) {
+	if ( ! token ) {
 		return new Promise( ( resolve, reject ) => {
-			printError( chalk.yellow( 'You do not have a GitHub token configured.' ) );
-			printError( chalk.yellow( 'Please Generate one at https://github.com/settings/tokens' ) );
-			printError( chalk.green( 'Once you have a token, run `gitnews --save-token`' ) );
-			reject( 'GITNEWS_TOKEN was not set' );
+			reject( 'GitHub token is not available' );
 		} );
 	}
 	log( 'fetching notifications...' );
-	return fetch( 'https://api.github.com/notifications', getFetchInit() );
+	return fetch( 'https://api.github.com/notifications', getFetchInit( token ) );
 }
 
-function fetchNotificationSubjectUrl( notification ) {
+function fetchNotificationSubjectUrl( token, notification ) {
 	const url = getUrlApiUrl( notification );
 	log( `fetching notification url for ${ url }...` );
-	return fetch( url, getFetchInit() )
+	return fetch( url, getFetchInit( token ) )
 		.then( convertToJson )
 		.then( subject => {
 			notification.htmlUrl = get( subject, 'html_url' );
@@ -114,63 +45,35 @@ function fetchNotificationSubjectUrl( notification ) {
 		} );
 }
 
-function fetchNotificationSubjectUrls( notifications ) {
+function fetchNotificationSubjectUrls( token, notifications ) {
 	log( `fetching notification urls for ${ notifications.length } notifications...` );
-	return Promise.all( notifications.map( fetchNotificationSubjectUrl ) );
-}
-
-function printError( err ) {
-	console.error( err );
+	return Promise.all( notifications.map( notification => fetchNotificationSubjectUrl( token, notification ) ) );
 }
 
 function checkForErrors( result ) {
 	return new Promise( ( resolve, reject ) => {
 		if ( result.message ) {
-			printError( chalk.red( 'An error occurred while trying to get your notifications:' ) );
 			return reject( result.message );
 		}
 		resolve( result );
 	} );
 }
 
-function fetchAndPrintNotifications() {
-	fetchNotifications()
-		.then( convertToJson )
-		.then( checkForErrors )
-		.then( fetchNotificationSubjectUrls )
-		.then( printNotifications )
-		.catch( printError );
-}
-
-function saveToken( token ) {
-	config.set( 'token', token );
-}
-
-function getToken() {
-	return process.env.GITNEWS_TOKEN || config.get( 'token' );
-}
-
 // -------------
 
-const cli = meow( `
-	Usage:
-		$ gitnews
-
-	Options:
-		--save-token  Prompt for the token and save it.
-		--verbose     Say what we're doing.
-` );
-
-if ( cli.flags.verbose ) {
-	logMessages = true;
+function getNotifications( token ) {
+	return fetchNotifications( token )
+		.then( convertToJson )
+		.then( checkForErrors )
+	// TODO: curry the fetch methods
+		.then( notifications => fetchNotificationSubjectUrls( token, notifications ) );
 }
 
-if ( cli.flags.saveToken ) {
-	output( chalk.yellow( 'Please Generate a token at https://github.com/settings/tokens' ) );
-	inquirer.prompt( { type: 'password', name: 'token', message: 'Enter the GitHub token:' } )
-	// TODO: verify the token before saving
-		.then( input => saveToken( input.token ) )
-		.then( () => output( chalk.green( 'The token was saved! Now you can run gitnews to get your notifications.' ) ) );
-} else {
-	fetchAndPrintNotifications();
+function setLogger( logger ) {
+	logFunction = logger;
 }
+
+module.exports = {
+	setLogger,
+	getNotifications,
+};
