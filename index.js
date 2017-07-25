bf014beed5988d0464543bc319395039aa9a87b1
Payton Swick <payton@foolord.com>
2017-07-25 12:19:40 -0400

    Defensively handle missing API response fields

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const md5Hex = require( 'md5-hex' );
+const get = require( 'lodash.get' );
 const { setLogger } = require( './lib/logger' );
 const { setFetchFunction } = require( './lib/fetch' );
 const { fetchNotifications, getAdditionalDataFetcher } = require( './lib/fetchers' );
 
 function convertToGitnews( notifications ) {
 	return notifications.map( apiData => {
+		if ( ! apiData ) {
+			apiData = {};
+		}
 		return {
 			api: {
 				notification: apiData,
 				subject: null, // will be filled-in later
 				comment: null, // will be filled-in later
 			},
-			id: md5Hex( apiData.id + apiData.updated_at ),
+			id: md5Hex( get( apiData, 'id', 0 ) + get( apiData, 'updated_at', '1' ) ),
 			unread: apiData.unread,
-			title: apiData.subject.title,
-			type: apiData.subject.type,
+			title: get( apiData, 'subject.title' ),
+			type: get( apiData, 'subject.type' ),
 			updatedAt: apiData.updated_at,
 			'private': apiData.private,
-			repositoryName: apiData.repository.name,
-			repositoryFullName: apiData.repository.full_name,
-			repositoryOwnerAvatar: apiData.repository.owner.avatar_url,
+			repositoryName: get( apiData, 'repository.name' ),
+			repositoryFullName: get( apiData, 'repository.full_name' ),
+			repositoryOwnerAvatar: get( apiData, 'repository.owner.avatar_url' ),
 			subjectUrl: null, // will be filled-in later
 			commentUrl: null, // will be filled-in later
 			commentAvatar: null, // will be filled-in later
