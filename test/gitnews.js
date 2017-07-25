e4a5ee4f23e2e6b65aefaf19a41a2d98c8162d55
Payton Swick <payton@foolord.com>
2017-07-25 12:29:49 -0400

    Add tests for unique IDs

--- test/gitnews.js
+++ test/gitnews.js
@@ -20,5 +20,24 @@ describe( 'gitnews', function() {
 					expect( results ).to.have.length( 2 );
 				} );
 		} );
+
+		it( 'resolves with notifications that each have a unique id with invalid data', function() {
+			setFetchFunction( getMockFetch( [ {}, {} ] ) );
+			return getNotifications( '123abc' )
+				.then( results => {
+					expect( results[ 0 ].id ).to.not.equal( results[ 1 ].id );
+				} );
+		} );
+
+		it( 'resolves with notifications that each have a unique id with valid data', function() {
+			setFetchFunction( getMockFetch( [
+				{ id: 5, updated_at: '123454' }, // eslint-disable-line camelcase
+				{ id: 6, updated_at: '123456' }, // eslint-disable-line camelcase
+			] ) );
+			return getNotifications( '123abc' )
+				.then( results => {
+					expect( results[ 0 ].id ).to.not.equal( results[ 1 ].id );
+				} );
+		} );
 	} );
 } );
