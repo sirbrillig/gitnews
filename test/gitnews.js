720cfcc4695434969d97fa47b56572806e3308bd
Payton Swick <payton@foolord.com>
2017-07-25 12:55:46 -0400

    Add tests for failed network connections

--- test/gitnews.js
+++ test/gitnews.js
@@ -2,17 +2,27 @@
 const { expect } = require( 'chai' );
 const { getNotifications, setFetchFunction } = require( '../index' );
 
-function getMockFetch( responseJson ) {
-	const mockResponse = { json: () => responseJson };
-	return function mockFetch() {
-		return new Promise( ( resolve ) => {
-			resolve( mockResponse );
-		} );
+function getMockFetch( responseJson, statusData = {} ) {
+	const status = statusData.status || 200;
+	const mockResponse = {
+		ok: status > 199 && status < 300,
+		status,
+		statusText: statusData.statusText || 'OK',
+		json: () => Promise.resolve( responseJson ),
 	};
+	return () => Promise.resolve( mockResponse );
 }
 
 describe( 'gitnews', function() {
 	describe( 'getNotifications()', function() {
+		it( 'rejects if the server returns an http error', function( done ) {
+			setFetchFunction( getMockFetch( [ {} ], { status: 400 } ) );
+			getNotifications( '123abc' )
+				.catch( () => {
+					done();
+				} );
+		} );
+
 		it( 'resolves with an array of notifications', function() {
 			setFetchFunction( getMockFetch( [ {}, {} ] ) );
 			return getNotifications( '123abc' )
